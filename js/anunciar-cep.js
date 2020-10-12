2c57773f2cf5a378c4076da96a1d38e49096a20e
Eduardo Perottoni <edu.perottoni@gmail.com>
2020-10-11 22:10:12 -0300

    Iniciando integração com Via CEP e validação de formulário em anunciar

--- js/anunciar-cep.js
+++ js/anunciar-cep.js
@@ -1,4 +1,26 @@
-const input = document.querySelector('#street')
+const endpoint = "https://viacep.com.br/ws/value/json/";
 
-input.value = 'Caxias do Sul'
+const ruaBairro = document.querySelector('#street')
+const cityEstado = document.querySelector('#city')
+
+
+const inputList =  ruaBairro;
+
+ruaBairro.addEventListener('blur',event => {
+    event.preventDefault();
+
+    const url = endpoint.replace('value',ruaBairro.value);
+    console.log(url)
+
+    const location = this.getCpf(url);
+    
+
+})
+
+
+async function getCpf(url){
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+}
 
